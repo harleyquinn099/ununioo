fbb2cb30bc7a8ca1d240ef08e2359b5ab33c5e44
Film gallery <88760900+Kiran7994@users.noreply.github.com>
2021-08-19 11:43:23 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,7 +23,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = ("<code>" + file_name + "</code>""""\n\n\n<b>Cʜᴀɴɴᴇʟ:- @Fantasy_Movies</b>""")
+        caption = ("<code>" + file_name + "</code>""""\n\n\n<b>Cʜᴀɴɴᴇʟ:- @unlimtedmovie00</b>""")
         
         if file_type == "document":
         
