ce051aee9e217187c1d24f70e6cb61d21fd65959
HARLEY QUINN <88760900+harleyquinn099@users.noreply.github.com>
2021-09-30 09:12:39 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,7 +23,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = ("<code>" + file_name + "</code>""""\n\n\n<b>𝐂𝐇𝐀𝐍𝐍𝐄𝐋:- @unlimtedmovie00</b>""")
+        caption = ("<code>" + file_name + "</code>""""\n\n\n<𝐅𝐎𝐑 𝐌𝐎𝐑𝐄 𝐌𝐎𝐕𝐈𝐄𝐒 𝐉𝐎𝐈𝐍 𝐈𝐍 𝐎𝐔𝐑 𝐂𝐇𝐀𝐍𝐍𝐄𝐋:- @unlimtedmovie00</b>""")
         
         if file_type == "document":
         
