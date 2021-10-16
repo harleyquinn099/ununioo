57af434ca531f9a8b1e6a563fb11eccf260f35c4
HARLEY QUINN <88760900+harleyquinn099@users.noreply.github.com>
2021-10-16 19:46:12 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -17,6 +17,8 @@ API_HASH = os.environ.get("API_HASH")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+CHANNEL = os.environ.get("CHANNEL")
+
 DB_URI = os.environ.get("DB_URI")
 
 USER_SESSION = os.environ.get("USER_SESSION")
