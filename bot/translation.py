60604629be0832ef67597efb8364f9e86db0d783
HARLEY QUINN <88760900+harleyquinn099@users.noreply.github.com>
2021-10-16 16:26:47 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -19,12 +19,3 @@ class Translation(object):
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Advance Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/uniunioo">hector</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/harleyquinn099/auto-filter-bot-v2">just happy</a></i>
-"""
