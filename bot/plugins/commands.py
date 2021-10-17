dd1874c785eabb3f5c0ff1cfe2b1ecbc6ffd8dba
HARLEY QUINN <88760900+harleyquinn099@users.noreply.github.com>
2021-10-17 11:17:46 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -13,6 +13,7 @@ db = Database()
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
     CHANNEL = justbot
+    if update_channel:
     try:
         user = await bot.get_chat_member(update_channel, update.chat.id)
             if user.status == "kicked out":
